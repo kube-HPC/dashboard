33bd48d83fe3d5d03534090da5a583d21a7135b8
denvash <justdenva@gmail.com>
2020-01-16 22:04:53 +0200

    [+] Icons

--- src/components/atoms/Icons/index.js
+++ src/components/atoms/Icons/index.js
@@ -3,7 +3,6 @@ import IconDev from './Dev/Dev.svg';
 import IconGithub from './Github/Github.svg';
 import iconMapper from './iconMapper';
 import IconLinkedin from './Linkedin/Linkedin.svg';
-import IconLogo from './Logo/Logo.svg';
 import IconMedium from './Medium/Medium.svg';
 import IconStackOverflow from './Stackoverflow/Stackoverflow.svg';
 import IconTwitter from './Twitter/Twitter.svg';
@@ -11,7 +10,6 @@ import IconTwitter from './Twitter/Twitter.svg';
 export default iconMapper;
 
 export {
-  IconLogo,
   IconGithub,
   IconLinkedin,
   IconStackOverflow,
