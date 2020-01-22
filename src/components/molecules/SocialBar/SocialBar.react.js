2ad12e515d0234e26fd8909574b0c618e90e2fdd
denvash <justdenva@gmail.com>
2020-01-23 00:43:38 +0200

    [+] animate social bar

--- src/components/molecules/SocialBar/SocialBar.react.js
+++ src/components/molecules/SocialBar/SocialBar.react.js
@@ -1,25 +1,42 @@
 import iconMapper from '@icons';
-import { mixins, styled } from '@styles';
+import { mixins, styled, theme } from '@styles';
 import React from 'react';
+import { animated, config, useTrail } from 'react-spring';
 import tw from 'tailwind.macro';
 
 const Container = styled.div`
   ${mixins.flexCenter}
   svg {
-    ${tw`w-full`}
+    ${tw`w-full fill-current p-5`}
   }
 `;
 
-const { CodeSandbox, Twitter, StackOverflow, Github } = iconMapper;
+const { CodeSandbox, Twitter, Github, StackOverflow } = iconMapper;
 
-const icons = [CodeSandbox, Twitter, StackOverflow, Github];
+const items = [CodeSandbox, Twitter, StackOverflow, Github];
 
-const SocialBar = () => (
-  <Container>
-    {icons.map((Icon, key) => (
-      <Icon key={key} />
-    ))}
-  </Container>
-);
+const SocialBar = () => {
+  const trail = useTrail(items.length, {
+    config: config.slow,
+    x: `0em`,
+    width: `20em`,
+    from: { color: theme.palette.primary, x: `10em`, width: `0em` },
+  });
+
+  return (
+    <Container>
+      {trail.map(({ width, x }, index) => {
+        const Icon = items[index];
+        return (
+          <animated.div key={index} style={{ transform: x.to(x => `translate3d(${x},0,0)`) }}>
+            <animated.div style={{ width: width }}>
+              <Icon />
+            </animated.div>
+          </animated.div>
+        );
+      })}
+    </Container>
+  );
+};
 
 export default SocialBar;
