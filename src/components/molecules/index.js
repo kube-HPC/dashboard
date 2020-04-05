6a26956edc47966b5d63402680ebfc6b060deaf1
denvash <justdenva@gmail.com>
2020-04-05 17:42:10 +0300

    [+] Notification molecule

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,6 +1,6 @@
 export { default as IconsBar } from './IconsBar/IconsBar.react';
 export { JobEntry, JobGraph } from './Job';
 export { default as Menu } from './Menu/Menu.react';
-export { default as Notification } from './Notification/Notification.react';
+export { default as Notifications } from './Notifications/Notifications.react';
 export { default as Panel } from './Panel/Panel.react';
 export { default as Welcome } from './Welcome/Welcome.react';
